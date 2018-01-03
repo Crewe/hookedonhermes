d1bb0a574f7436b3a0afa0b971601b2682b19e3f
Ryan Edwards-Crewe <Crewe@users.noreply.github.com>
2018-01-03 15:46:32 -0500

    Update hermes.py
    Added main()

--- hermes.py
+++ hermes.py
@@ -29,52 +29,57 @@ RHYMES = [
     ['typeface', 'outer space']
 ]
 
-x = random.randrange(0, len(ADJECTIVES))
-y = random.randrange(0, len(RHYMES))
-phrase = "{adj} {something} of {someplace}!".format(
-    adj=ADJECTIVES[x], 
-    something=RHYMES[y][0], 
-    someplace=RHYMES[y][1])
+def main():
+    x = random.randrange(0, len(ADJECTIVES))
+    y = random.randrange(0, len(RHYMES))
+    phrase = "{adj} {something} of {someplace}!".format(
+        adj=ADJECTIVES[x],
+        something=RHYMES[y][0],
+        someplace=RHYMES[y][1])
 
-payload = {'text': phrase,
+    payload = {'text': phrase,
            'username': 'Hermes Conrad',
            'channel': DEFAULT_CHANNEL}
+ 
+    # Setup a custom emoji in Slack or use a URL to an image.
+    # You may only use one or the other.
+    # payload['icon_emoji'] = ':hermes:'
+    # payload['icon_url'] = ICON_URL
 
-# Setup a custom emoji in Slack or use a URL to an image.
-# You may only use one or the other.
-# payload['icon_emoji'] = ':hermes:'
-# payload['icon_url'] = ICON_URL
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:],
-    "hc:u:t:p:s:",
-    ["help","channel=","text=","pull-request=", "subject="])
-except getopt.GetoptError:
-    print ('usage: hermes.py [-c <channel>] [-u <user>] [-t=<additional text>] [-p=<pull request URL> [-s=<subject>]]')
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+        "hc:u:t:p:s:",
+        ["help","channel=","text=","pull-request=", "subject="])
+    except getopt.GetoptError:
         print ('usage: hermes.py [-c <channel>] [-u <user>] [-t=<additional text>] [-p=<pull request URL> [-s=<subject>]]')
-        sys.exit()
-    elif opt in ("-c", "--channel"):
-        payload['channel'] = "#" + arg
-    elif opt in ("-u", "--user"):
-        payload['channel'] = "@" + arg 
-    elif opt in ("-t", "--text"):
-        payload['text'] = "{} {}".format(phrase, arg)
-    elif opt in("-p", "--pull-request"):
-        payload['attachments'] = [
-            {'color': '#7FF7EC',
-            'pretext': 'There\'s a new PR in the books mon.',
-            'title': 'TFS Pull Request', 
-            'footer': 'PR Express',
-            'footer_icon': THUMB_URL,
-            'text': arg}]
-        for opt2, arg2 in opts:
-            if opt2 in ("-s", "--subject"):
-                payload['attachments'][0]['title'] = arg2
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('usage: hermes.py [-c <channel>] [-u <user>] [-t=<additional text>] [-p=<pull request URL> [-s=<subject>]]')
+            sys.exit()
+        elif opt in ("-c", "--channel"):
+            payload['channel'] = "#" + arg
+        elif opt in ("-u", "--user"):
+            payload['channel'] = "@" + arg
+        elif opt in ("-t", "--text"):
+            payload['text'] = "{} {}".format(phrase, arg)
+        elif opt in("-p", "--pull-request"):
+            payload['attachments'] = [
+                {'color': '#7FF7EC',
+                'pretext': 'There\'s a new PR in the books mon.',
+                'title': 'Pull Request',
+                'footer': 'PR Express',
+                'footer_icon': THUMB_URL,
+                'text': arg}]
+            for opt2, arg2 in opts:
+                if opt2 in ("-s", "--subject"):
+                    payload['attachments'][0]['title'] = arg2
 
-bin_data = json.dumps(payload,  separators=(',',':')).encode('ascii')
-req = request.Request(WEBHOOK_URL, method='POST', data=bin_data)
-req.add_header('Content-Type', 'application/json')
-resp = request.urlopen(req)
+    bin_data = json.dumps(payload,  separators=(',',':')).encode('ascii')
+    req = request.Request(WEBHOOK_URL, method='POST', data=bin_data)
+    req.add_header('Content-Type', 'application/json')
+    resp = request.urlopen(req)
+
+
+if __name__ == "__main__":
+    main()
